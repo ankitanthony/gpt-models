4badf6f3488dfe77052de715e865e24b850690f7
ankitanthony <106681349+ankitanthony@users.noreply.github.com>
2023-11-09 00:45:50 +0530

    Add files via upload

---
+++ Code Explanation .py
@@ -0,0 +1,27 @@
+import openai
+openai.api_key="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"
+#method to print and update the message list with repsonse from gpt model.
+
+def get_gpt_response(messages):
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=messages
+)
+    print(response['choices'][0]['message']['content'])
+    return response['choices'][0]['message']['content']
+#method to update the message list with user prompt.
+
+def update_chat(messages,role,content):
+    messages.append({'role':role,'content':content})
+    return messages
+
+messages=[
+        {"role": "system", "content": "You are a trained to stepwise explain the code snippets."}
+         ]
+print(messages[0]['content'])
+while True:
+    user_input=input()
+    messages=update_chat(messages,'user',user_input)
+    model_response=get_gpt_response(messages)
+    messages=update_chat(messages,'assistant',model_response)
+
